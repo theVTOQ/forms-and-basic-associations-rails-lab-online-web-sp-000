074beb64d40879a7fbc8d58c7629829cda131858
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 21:35:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,7 +15,7 @@ class Song < ActiveRecord::Base
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
-  
+
   def genre_name
     self.genre ? self.genre.name : nil
   end
