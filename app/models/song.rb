6b6761f6df554021b687f8a561f450cd4f2db8d2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 21:34:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,10 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
   def artist_name
     self.artist ? self.artist.name : nil
   end
