f9556a6c3df241ecab89937a2b24e0cd80918c76
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-23 21:21:15 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,4 +7,11 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def notes_contents=(contents)
+    contents.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note
+    end
+  end
 end
